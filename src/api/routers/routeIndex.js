c2f803c0b30dd4ff398c860644090a71df943faf
AlkaSinha5 <sinhaalka802211@gmail.com>
2024-07-14 12:41:13 +0530

    banner

--- src/api/routers/routeIndex.js
+++ src/api/routers/routeIndex.js
@@ -5,7 +5,8 @@ const roleRoutes = require('./roleRoute');
 const bankRoutes = require('./bankDetail');
 const upiRoutes = require('./upiRoute');
 const withdrowRoutes =require('./withdrow');
-const walletRoute =require('./wallet')
+const walletRoute =require('./wallet');
+const bannerRoute = require('./bannerRoute')
 
 const allRouters = express.Router();
 
@@ -15,4 +16,5 @@ allRouters.use("/bankDetail", bankRoutes);
 allRouters.use("/upiDetail", upiRoutes);
 allRouters.use("/withDrow",withdrowRoutes);
 allRouters.use("/wallet",walletRoute);
+allRouters.use("/banner",bannerRoute);
 module.exports =  allRouters;
