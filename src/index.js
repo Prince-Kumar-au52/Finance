5ff92340ccc688ce494ef18f056adcd288713f0d
shobhakumari108 <shobhakumari138484@gmail.com>
2024-07-18 11:32:58 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ app.use(fileUpload({
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(cors());
+app.use(cors({ origin: "https://mercury-uat.phonepe.com" }));
 
 //routes
 app.use("/v1", allRouters);
