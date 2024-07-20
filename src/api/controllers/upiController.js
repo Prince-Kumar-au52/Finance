facf47fbca27fb484779534a9c1919e936340eeb
AlkaSinha5 <sinhaalka802211@gmail.com>
2024-07-20 13:05:33 +0530

    upi update done

--- src/api/controllers/upiController.js
+++ src/api/controllers/upiController.js
@@ -83,6 +83,24 @@ exports.getUPIDetailById = async (req, res) => {
       .send({ statusCode: 500, error: error.message, success: false });
   }
 };
+exports.getUPIDetailByUserId = async (req, res) => {
+  try {
+    const upi = await UPIDetail.findOne({ CreatedBy: req.params.id });
+    if (!upi) {
+      return res
+        .status(404)
+        .json({ error: "UPIDetail not found", success: false });
+    }
+    return res
+      .status(200)
+      .send({ statusCode: 200, data: upi, success: true });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ statusCode: 500, error: error.message, success: false });
+  }
+};
+
 
 exports.updateUPIDetail = async (req, res) => {
   try {
@@ -103,7 +121,27 @@ exports.updateUPIDetail = async (req, res) => {
       .send({ statusCode: 500, error: error.message, success: false });
   }
 };
-
+exports.updateUPIDetailByUser = async (req, res) => {
+  try {
+    const upi = await UPIDetail.findOneAndUpdate(
+      { CreatedBy: req.params.id },
+      req.body,
+      { new: true }
+    );
+    if (!upi) {
+      return res
+        .status(404)
+        .json({ error: "UPIDetail not found", success: false });
+    }
+    res
+      .status(constants.status_code.header.ok)
+      .send({ statusCode: 200, message: constants.curd.update, success: true });
+  } catch (error) {
+    return res
+      .status(constants.status_code.header.server_error)
+      .send({ statusCode: 500, error: error.message, success: false });
+  }
+};
 exports.deleteUPIDetail = async (req, res) => {
   try {
     const upi = await UPIDetail.findByIdAndUpdate(req.params.id, {
